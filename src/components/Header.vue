3cdd9b04c54b7c2240fbec22c0e71fe4b6174aa0
sixiaofeng <sxf754872493@gmail.com>
2018-12-23 21:47:21 +0800

    初始化

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="header">
+    <img src="#" alt="">
+    <ul>
+      <li><a href="#">首页</a></li>
+      <li><a href="#">新手入门</a></li>
+      <li><a href="#">API</a></li>
+      <li><a href="#">关于</a></li>
+      <li><a href="#">登录</a></li>
+      <li><a href="#">注册</a></li>
+    </ul>
+  </div>
+
+</template>
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style scoped>
+
+</style>
